b582ce5c384ebca674dec57683957d1475eaaf29
kibethh <kibethh@gmail.com>
2023-10-17 01:10:26 +0300

    init commit

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use std::fmt;
+use std::io::{Error, ErrorKind};
+use std::str::FromStr;
+
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+#[derive(Debug)]
+struct QuestionId(String);
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
+impl std::fmt::Display for Question {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        write!(
+            f,
+            "{}, title: {}, content: {}, tags: {:?}",
+            self.id, self.title, self.content, self.tags
+        )
+    }
+}
+impl std::fmt::Display for QuestionId {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        write!(f, "id: {}", self.0)
+    }
+}
+impl std::fmt::Debug for Question {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(f, "{:?}", self.tags)
+    }
+}
+
+fn main() {
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id provided"),
+        "First Question".to_string(),
+        "Content of question".to_string(),
+        Some(vec!["faq".to_string()]),
+    );
+    println!("{:?}", question);
+}
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
+        }
+    }
+}
