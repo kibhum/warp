392e1003441d7b32f568d772acd3ca4c2d59ceb1
kibethh <kibethh@gmail.com>
2023-10-21 00:11:18 +0300

    Added more endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
+use serde::Serialize;
 use std::fmt;
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
-use warp::Filter;
+use warp::{
+    filters::cors::CorsForbidden, http::Method, http::StatusCode, reject::Reject, Filter,
+    Rejection, Reply,
+};
 
+#[derive(Debug, Serialize)]
 struct Question {
     id: QuestionId,
     title: String,
     content: String,
     tags: Option<Vec<String>>,
 }
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct QuestionId(String);
 
 impl Question {
@@ -23,30 +28,63 @@ impl Question {
     }
 }
 
-impl std::fmt::Display for Question {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
-        write!(
-            f,
-            "{}, title: {}, content: {}, tags: {:?}",
-            self.id, self.title, self.content, self.tags
-        )
-    }
-}
-impl std::fmt::Display for QuestionId {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
-        write!(f, "id: {}", self.0)
-    }
-}
-impl std::fmt::Debug for Question {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
-        write!(f, "{:?}", self.tags)
+#[derive(Debug)]
+struct InvalidId;
+impl Reject for InvalidId {}
+
+// impl std::fmt::Display for Question {
+//     fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+//         write!(
+//             f,
+//             "{}, title: {}, content: {}, tags: {:?}",
+//             self.id, self.title, self.content, self.tags
+//         )
+//     }
+// }
+// impl std::fmt::Display for QuestionId {
+//     fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+//         write!(f, "id: {}", self.0)
+//     }
+// }
+// impl std::fmt::Debug for Question {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+//         write!(f, "{:?}", self.tags)
+//     }
+// }
+
+async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id provided"),
+        "First Question".to_string(),
+        "Content of question".to_string(),
+        Some(vec!["faq".to_string()]),
+    );
+
+    match question.id.0.parse::<i32>() {
+        Err(_) => Err(warp::reject::custom(InvalidId)),
+        Ok(_) => Ok(warp::reply::json(&question)),
     }
 }
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::get().map(|| format!("Hello, World!"));
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+    let cors = warp::cors()
+        .allow_any_origin()
+        // .allow_header("content-type")
+        .allow_header("not-in-the-request")
+        .allow_methods(&[Method::PUT, Method::DELETE, Method::GET, Method::POST]);
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        // Uses path::end to signal that we listen on
+        // exactly /question (and not /question/further/
+        // params, for example)
+        .and(warp::path::end())
+        .and_then(get_questions)
+        .recover(return_error);
+    //Defines the routes variable,
+    // which will come in handy later
+    let routes = get_items.with(cors);
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
 
 impl FromStr for QuestionId {
@@ -56,5 +94,25 @@ impl FromStr for QuestionId {
             false => Ok(QuestionId(id.to_string())),
             true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
         }
+    }
+}
+
+async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
+    println!("{:?}", r);
+    if let Some(error) = r.find::<CorsForbidden>() {
+        Ok(warp::reply::with_status(
+            error.to_string(),
+            StatusCode::FORBIDDEN,
+        ))
+    } else if let Some(_InvalidId) = r.find::<InvalidId>() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented".to_string(),
+            StatusCode::UNPROCESSABLE_ENTITY,
+        ))
+    } else {
+        Ok(warp::reply::with_status(
+            "Route not found".to_string(),
+            StatusCode::NOT_FOUND,
+        ))
     }
 }
